a500dfa5a2880c15f59a6fd78eaeb5db185cbad4
Nguyễn Lê Đăng Khoa <78264245+nguyenkhoakis@users.noreply.github.com>
2021-02-23 13:49:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -51,9 +51,9 @@
     }
     
      function botResponse() {
-     const r = random(0, BOT_MSGS.length + 1000);
+     const r = random(1000, BOT_MSGS.length * 100);
       const msgText = BOT_MSGS[r];
-      const delay = msgText.split(" ").length * 20) * 100);
+      const delay = msgText.split(" ").length * 20;
     
       setTimeout(() => {
         appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
